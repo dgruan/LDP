2f5fef098664a972292790bbb909908b82dfdf74
dgruan <douglasruan27@outlook.com>
2026-06-13 15:29:13 -0300

    Create Pares, Ímpares, Positivos e Negativos.c

---
+++ C/4ª lista/Pares, Ímpares, Positivos e Negativos.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+
+    int par = 0, impar = 0, positivo = 0, negativo = 0, n;
+
+    for(int i = 0; i < 5; i++){
+        scanf("%d", &n);
+
+        if(n % 2 == 0){
+            par++;
+        } else {
+            impar++;
+        }
+
+        if(n > 0){
+            positivo++;
+        } else if(n < 0){
+            negativo++;
+        }
+    }
+
+    printf("%d valor(es) par(es)\n", par);
+    printf("%d valor(es) impar(es)\n", impar);
+    printf("%d valor(es) positivo(s)\n", positivo);
+    printf("%d valor(es) negativo(s)\n", negativo);
+
+    return 0;
+}
